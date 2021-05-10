7980d7377d8cd8c9a285212486ed49b79d5f0042
NickMateus11 <mateus.nicholas@gmail.com>
2021-05-10 14:58:38 -0400

    black and white output only

--- floyd-steinburg_dithering.py
+++ floyd-steinburg_dithering.py
@@ -6,7 +6,7 @@ IMGFILE = 'test.png'
 
 
 def find_closest_pallete_color(pixel): # black and white
-    return 255 if pixel[0]>=128 else 0
+    return 255 if max(pixel)>=128 else 0
 
 
 def main():
@@ -15,16 +15,18 @@ def main():
         w,h = im.size
         pixels = im.load()
 
+    print("Computing ...")
+
     for y in range(1, h-1):
         for x in range(1, w-1):
             oldpixel = pixels[x, y]
             newpixel = find_closest_pallete_color(oldpixel)
             pixels[x, y] = (newpixel,)*3
-            quant_error = oldpixel[0] - newpixel
-            pixels[x + 1, y    ] = (pixels[x + 1, y    ][0] + int(quant_error * 7 / 16),)*3
-            pixels[x - 1, y + 1] = (pixels[x - 1, y + 1][0] + int(quant_error * 3 / 16),)*3
-            pixels[x    , y + 1] = (pixels[x    , y + 1][0] + int(quant_error * 5 / 16),)*3
-            pixels[x + 1, y + 1] = (pixels[x + 1, y + 1][0] + int(quant_error * 1 / 16),)*3
+            quant_error = max(oldpixel) - newpixel
+            pixels[x + 1, y    ] = (max(pixels[x + 1, y    ]) + int(quant_error * 7 / 16),)*3
+            pixels[x - 1, y + 1] = (max(pixels[x - 1, y + 1]) + int(quant_error * 3 / 16),)*3
+            pixels[x    , y + 1] = (max(pixels[x    , y + 1]) + int(quant_error * 5 / 16),)*3
+            pixels[x + 1, y + 1] = (max(pixels[x + 1, y + 1]) + int(quant_error * 1 / 16),)*3
     
     im.show() 
 
